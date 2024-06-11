04c8ca341ebb33e18f7a1502eec483445e87dac4
sahmal437 <78097031+sahmal437@users.noreply.github.com>
2024-06-11 22:17:09 +0500

    myfirstcommit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TicTacToe from './Components/Tic-Tac-Toe/tic-tac-toe';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      
+      <TicTacToe />
+    </div>
+  );
+}
